687620a0a2b5e5b47abd273cf37cbd2e57df0a40
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-04-30 17:53:42 +0000

    individual positions

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 
 
+segment_1 = Turtle("square")
+segment_1.color("white")
+
+segment_2 = Turtle("square")
+segment_2.color("white")
+segment_2.goto(-20, 0)
+
+segment_3 = Turtle("square")
+segment_3.color("white")
+segment_3.goto(-40, 0)
 
 
 
