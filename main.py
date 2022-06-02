2550c030b0c09b29696fb276474dc89fc1a7e26f
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-06-02 17:48:15 +0000

    create food and detect when snake collides with food

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,35 +9,24 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
+food = Food()
 
-segments = []
-
-for position in starting_positions:
-  new_segment = Turtle("square")
-  new_segment.color("white")
-  new_segment.penup()
-  new_segment.goto(position)
-  segments.append(new_segment)
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_on = True
 while game_on:
   screen.update()
-  time.sleep(0.5)
-  
-  for seg_num in range(len(segments) - 1, 0, -1):
-    new_x = segments[seg_num - 1].xcor()
-    new_y = segments[seg_num - 1].ycor()
-    segments[seg_num].goto(new_x, new_y)
+  time.sleep(0.2)
 
-  segments[0].forward(20)
+  snake.move()
 
-  
-    
-    
-  
-
-
+  # Detect collision with food
+  if snake.head.distance(food) < 15:
+    food.refresh()
 
 screen.exitonclick()
