f5326b64deea157bd94d43fbf4cee4a81d22eb5d
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-06-17 15:38:05 +0000

    Add High Score to Game

--- main.py
+++ main.py
@@ -31,20 +31,20 @@ while game_on:
   # Detect collision with food
   if snake.head.distance(food) < 15:
     food.refresh()
+    snake.extend()
     scoreboard.increase_score()
-    scoreboard.game_over()
 
   # Detect collision with wall
   if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-    game_on = False
-    scoreboard.game_over()
+    scoreboard.reset()
+    snake.reset()
 
   # Detect collision with tail
   # if head collides with any part of the body
   for segment in snake.segments[1:]:
     if snake.head.distance(segment) < 10:
-      game_on = False
-      scoreboard.game_over()
+      scoreboard.reset()
+      snake.reset()
   
   
 
